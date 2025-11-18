974db5feded0c701a4a7c70d3558503ab66f3f8e
Jean Runnells <jrunnells@mekyd.com>
2025-11-18 15:59:32 -0500

    Commit from TeleportHQ

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,7 +7,7 @@ import { Title, Meta } from '@angular/platform-browser'
   styleUrls: ['home.component.css'],
 })
 export class Home {
-  rawc7zk: string = ' '
+  rawa53l: string = ' '
   constructor(private title: Title, private meta: Meta) {
     this.title.setTitle('MEKYD')
     this.meta.addTags([
